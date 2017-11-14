e7344a9203777424db99608be257193fc9db8b26
Ryan Fadholi <rynfdhl@gmail.com>
2017-11-14 19:51:36 +0700

    Add request adapter

---
+++ app/src/main/java/com/mobcomunsri2017/bergerakbersamamu/projectmobcom/SongRequestAdapter.java
@@ -0,0 +1,16 @@
+package com.mobcomunsri2017.bergerakbersamamu.projectmobcom;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by rynfd on 11/4/2017.
+ */
+
+public class SongRequestAdapter extends RecyclerView.Adapter<SongRequestAdapter.SongRequestHolder> {
+
+    private Context mContext;
+    private List<SongRequest> requestList;
+
+    public interface
+}
