98137cd9a50980b5939172106fb8c6a0a8c981ca
Muhammad Ryan Fadholi <rynfdhl@gmail.com>
2017-11-15 12:32:23 +0700

    Added some visual garnishes

--- app/src/main/java/com/mobcomunsri2017/bergerakbersamamu/projectmobcom/PlaylistActivity.java
+++ app/src/main/java/com/mobcomunsri2017/bergerakbersamamu/projectmobcom/PlaylistActivity.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 public class PlaylistActivity extends AppCompatActivity {
 
     public static final String BASE_WEB_SERVICE_URL = "http://192.168.0.169/mpcafe/";
-//    public static final String BASE_WEB_SERVICE_URL = "http://192.168.43.144/mpcafe/";
 //    public static final String BASE_WEB_SERVICE_URL = "http://192.168.0.123/mpcafe/";
 
     private static String LOG_TAG = "PV.MainActivity";
@@ -59,6 +58,20 @@ public class PlaylistActivity extends AppCompatActivity {
 
         songs.add(new Song("Partai Perindo", "Hary Tanoesoedibjo", "Mars Perindo", 20));
         songs.add(new Song("Unknown Album", "Unknown Artist", "Kapten Tsubasa", 20));
+        songs.add(new Song("Partai Perindo", "Hary Tanoesoedibjo", "Mars Perindo", 20));
+        songs.add(new Song("Unknown Album", "Unknown Artist", "Kapten Tsubasa", 20));
+        songs.add(new Song("Partai Perindo", "Hary Tanoesoedibjo", "Mars Perindo", 20));
+        songs.add(new Song("Unknown Album", "Unknown Artist", "Kapten Tsubasa", 20));
+        songs.add(new Song("Partai Perindo", "Hary Tanoesoedibjo", "Mars Perindo", 20));
+        songs.add(new Song("Unknown Album", "Unknown Artist", "Kapten Tsubasa", 20));
+        songs.add(new Song("Partai Perindo", "Hary Tanoesoedibjo", "Mars Perindo", 20));
+        songs.add(new Song("Unknown Album", "Unknown Artist", "Kapten Tsubasa", 20));
+        songs.add(new Song("Partai Perindo", "Hary Tanoesoedibjo", "Mars Perindo", 20));
+        songs.add(new Song("Unknown Album", "Unknown Artist", "Kapten Tsubasa", 20));
+        songs.add(new Song("Partai Perindo", "Hary Tanoesoedibjo", "Mars Perindo", 20));
+        songs.add(new Song("Unknown Album", "Unknown Artist", "Kapten Tsubasa", 20));
+
+
 
         playlistRecyclerView = (RecyclerView)findViewById(R.id.playlist);
         playlistRecyclerView.setHasFixedSize(true);
