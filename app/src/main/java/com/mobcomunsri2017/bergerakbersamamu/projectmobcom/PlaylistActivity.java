e9ce84668906f1011b50ed77d099ac7da19273e4
azhry <arliansyah_azhary@yahoo.com>
2017-11-28 10:30:12 +0700

    Change IP

--- app/src/main/java/com/mobcomunsri2017/bergerakbersamamu/projectmobcom/PlaylistActivity.java
+++ app/src/main/java/com/mobcomunsri2017/bergerakbersamamu/projectmobcom/PlaylistActivity.java
@@ -26,6 +26,7 @@ public class PlaylistActivity extends AppCompatActivity {
 
     public static final String BASE_WEB_SERVICE_URL = "http://192.168.0.169/mpcafe/";
 //    public static final String BASE_WEB_SERVICE_URL = "http://192.168.0.123/mpcafe/";
+//    public static final String BASE_WEB_SERVICE_URL = "http://10.102.224.74/mpcafe/";
 
     private static String LOG_TAG = "PV.MainActivity";
     private PlaylistService service;
